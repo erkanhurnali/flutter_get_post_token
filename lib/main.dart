a95bb5fd07c1763e433b5f371d8fdaae89c60acd
Erkan Hürnalı <erkanhurnali@gmail.com>
2023-10-04 09:42:10 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_get_post_token/views/login.dart';
+// import 'package:flutter_get_post_token/views/login.dart';
 import 'package:flutter_get_post_token/views/splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
